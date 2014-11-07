2358c55a4ed434f019106936d595de448f4d29f5
Sean Abbas <sean-abbas@uiowa.edu>
2014-11-06 23:09:11 -0600

    Added role security function and made other tweaks

--- Login.aspx.vb
+++ Login.aspx.vb
@@ -2,4 +2,12 @@
 Partial Class Login
     Inherits System.Web.UI.Page
 
+    Protected Sub Login1_LoggedIn(sender As Object, e As EventArgs) Handles Login1.LoggedIn
+
+        If Roles.IsUserInRole(Login1.UserName, "saabbas_admin") = True Then
+            Response.Redirect("~/admin/Default.aspx")
+        Else : Response.Redirect("~/Default.aspx")
+        End If
+
+    End Sub
 End Class
